130b1537c8872ec4283fd3b407f969acae473ff2
Karolina <karolinaduchnik17@wp.pl>
2020-05-09 15:24:39 +0200

    Add populateUI function with saved data

--- Movie_Seat_Booking/main.js
+++ Movie_Seat_Booking/main.js
@@ -8,12 +8,13 @@ let ticketPrice = +movieSelect.value;
 
 
 
+populateUI();
+
 //Save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
     localStorage.setItem("selectedMovieIndex", movieIndex);
     localStorage.setItem("selectedMoviePrice", moviePrice);
 }
-
 
 
 function updateSelectedCount() {
@@ -25,12 +26,28 @@ function updateSelectedCount() {
     //local storage
     localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
-
-
-    console.log(seatsIndex);
     count.innerText = selectedSeats.length;
 
     total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("selected")
+            }
+        })
+    }
+
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
 movieSelect.addEventListener("change", e => {
@@ -39,14 +56,12 @@ movieSelect.addEventListener("change", e => {
     updateSelectedCount();
 })
 
-
-
 container.addEventListener("click", e => {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
         e.target.classList.toggle("selected");
 
-
         updateSelectedCount();
     }
-
 })
+
+updateSelectedCount();
